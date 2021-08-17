cdd36e4fee0d49a3bdaf1a62eb629bce87566164
Prakhar222 <87750555+Prakhar222@users.noreply.github.com>
2021-08-17 22:51:15 +0530

    Add files via upload

---
+++ IO/FileDemo.java
@@ -0,0 +1,40 @@
+package com.yash.NewIo;
+/**
+*
+*@Prakhar Gupta
+*
+*
+ */
+import java.io.*;
+import java.io.FileInputStream;
+
+public class FileDemo {
+
+	public static void main(String[] args) 
+	{
+	try
+	{
+		File f=new File("demo1.txt");
+		
+		System.out.println(" File name:- "+f.getName()+ " File Path:- "
+		+ f.getPath()+ " File Parent:- "+ f.getParent()+ " File exist:- "
+		+ f.exists());
+		
+		FileInputStream in=new FileInputStream(f);
+		
+		int i;
+		
+		while((i=in.read())!=-1)
+			System.out.println((char)i);
+		FileReader f1=new FileReader(f);
+		while((i=f1.read())!=-1)
+			System.out.println((char)i);
+	}
+	
+	catch(Exception e)
+	{
+		System.out.println(e);
+	}
+	}
+
+}
